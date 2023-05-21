6fb08b22bff47f4add41a14a4b54956eaeef3fc6
franco <flaalf@gmail.com>
2023-05-21 00:11:50 -0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <div class="text-center">
+    <button type="button" class="btn btn-success">Success</button>
+  </div>
+</template>
+
+<style scoped>
+.unused-class {
+  color: #888;
+}
+</style>
